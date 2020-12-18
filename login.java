f23c41f6c108a3ef95c4782ab8300dce9b1a31ce
hagar-fouad <hagarabdelmonam1997@gmail.com>
2020-12-18 16:30:29 +0200

    h edit

--- login.java
+++ login.java
@@ -2,7 +2,7 @@
 class LoginPage{
 public static void main(String args[]){
 
-    System.out.println("Login Page by hagar");
+    System.out.println("Login Page by hagar fouad");
     System.out.println("And HelooWorld");
 
 }
