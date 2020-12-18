b0e02fbd9b6760467e0586f75eeb5f22fa473619
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2020-12-18 16:29:34 +0200

    Form ebrahim

--- login.java
+++ login.java
@@ -1,7 +1,7 @@
 
 class LoginPage{
 public static void main(String args[]){
-    System.out.println("Login Page by hagar");
+    System.out.println("Login Page by Ebrahim");
     System.out.println("And HelooWorld");
 
 }
